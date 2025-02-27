943bd8b08794bbbf46af0f4b1342d95baa79bbb4
SRS <rs.sku@yandex.ru>
2025-02-27 23:56:58 +0300

    Add m_2_t_11

---
+++ src/m_2_t_11.py
@@ -0,0 +1,19 @@
+def binary_search(arr: list[int | float], numb: int | float) -> bool:
+    left, right = 0, len(arr) - 1
+    while left <= right:
+        mid = (right + left) // 2
+        if numb == arr[mid]:
+            return True
+        elif numb > arr[mid]:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return False
+
+
+arr = [1, 2, 3, 45, 356, 569, 600, 705, 923]
+
+assert binary_search(arr, 356)
+assert binary_search(arr, 1)
+assert binary_search(arr, 923)
+assert not binary_search(arr, 42)
