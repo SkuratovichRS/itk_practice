385d5bc5c0753a4fd3f6867d9d031d8723fea8fb
SRS <rs.sku@yandex.ru>
2025-03-06 19:17:04 +0300

    Refactor m_2_t_2

--- src/week_2/m_3_t_2/app/views.py
+++ src/week_2/m_3_t_2/app/views.py
@@ -1,25 +1,32 @@
-from app.serializers import UserSerializer
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView, Response
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
+
+from app.serializers import UserSerializer
 
 
 class RegisterView(TokenObtainPairView):
     def post(self, request, *args, **kwargs):
         user_serializer = UserSerializer(data=request.data)
-        if user_serializer.is_valid():
-            user_serializer.save()
-            response = super().post(request, *args, **kwargs)
-            response.data["message"] = "User created successfully"
-            return response
-        return Response(user_serializer.errors, status.HTTP_400_BAD_REQUEST)
+        if not user_serializer.is_valid():
+            return Response(user_serializer.errors, status.HTTP_400_BAD_REQUEST)
+        user_serializer.save()
+        response = super().post(request, *args, **kwargs)
+        response.data["message"] = "User created successfully"
+        return response
+        
 
 
 class LogoutView(TokenRefreshView):
-    def post(self, request, *args, **kwargs):
-        super().post(request, *args, **kwargs)
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        try:
+            serializer.is_valid(raise_exception=True)
+        except TokenError as e:
+            raise InvalidToken(e.args[0])
         token = RefreshToken(request.data["refresh"])
         token.blacklist()
         return Response(
--- src/week_2/m_3_t_2/project/urls.py
+++ src/week_2/m_3_t_2/project/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from app.views import LogoutView, ProtectedView, RegisterView
 from django.contrib import admin
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from app.views import LogoutView, ProtectedView, RegisterView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
